d4c2d4d4b5a9f74588e7b3a9dace980282a39184
Vandanarajput <145121571+Vandanarajput@users.noreply.github.com>
2026-05-15 22:08:10 +0530

    init

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [react()],
-    base: '/Vandana_Portfolio/',
     build: {
         target: 'es2020',
         cssCodeSplit: true,
