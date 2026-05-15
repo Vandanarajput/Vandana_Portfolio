653f76850c9a4c4efbf91c68a520bdb01fd19384
Vandanarajput <145121571+Vandanarajput@users.noreply.github.com>
2026-05-15 22:30:21 +0530

    Updated something

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-export default defineConfig({
+
+export default defineConfig(({ mode }) => ({
     plugins: [react()],
+    base: mode === 'gh-pages' ? '/Vandana_Portfolio/' : '/',
     build: {
         target: 'es2020',
         cssCodeSplit: true,
     },
-});
+}));
